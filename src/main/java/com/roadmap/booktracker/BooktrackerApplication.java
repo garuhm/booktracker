5fa2e49b04560fa1a7e1713bc85e80f7dd63ef33
Gabriel Ruh-Mosquera <garuhmosquera@gmail.com>
2026-05-12 20:32:09 -0400

    removing unused config props

--- src/main/java/com/roadmap/booktracker/BooktrackerApplication.java
+++ src/main/java/com/roadmap/booktracker/BooktrackerApplication.java
@@ -1,7 +1,5 @@
 package com.roadmap.booktracker;
 
-import com.roadmap.booktracker.auth.config.properties.CookieProperties;
-import com.roadmap.booktracker.auth.config.properties.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -9,8 +7,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableConfigurationProperties({
-        JwtProperties.class,
-        CookieProperties.class
 })
 @EnableScheduling
 public class BooktrackerApplication {
